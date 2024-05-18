2f4ece9a1ed76531051e1ac3ae1dee000011bc31
taiiwin1 <tainnguyen2@gmail.com>
2024-05-17 18:59:39 -0700

    added router, navbar tabs work

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
 import {Header} from './components/Header';
 import {HomePage} from './components/HomePage';
 import {AboutPage} from './components/AboutPage';
 import {LoginPage} from './components/LoginPage';
+import {Footer} from './components/Footer';
 import './style.css';
 
 export function App() {
   return (
     <div>
       <Header />
-      <HomePage />
-      <AboutPage /> 
-      <LoginPage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
+
+// export function App() {
+//   return (
+//     <div>
+//       <Header />
+//       <HomePage />
+//       <AboutPage /> 
+//       <LoginPage />
+//     </div>
+//   );
+// }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../style.css';
+import { Link } from 'react-router-dom';
 
 export function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  let navigationClass = 'navigation';
+  if (menuOpen) {
+    navigationClass += ' active';
+  }
+
   return (
     <header>
-      <div className="menu-icon">
+      <div className="menu-icon" onClick={toggleMenu}>
         <img src="img/hamburger123.png" alt="the hamburger menu icon" />
       </div>
-      <div className="navigation">
-        <a href="index.html">Home</a>
-        <a href="about.html">About Us</a>
-        <a href="login.html">Login</a>
+      <div className={navigationClass}>
+        <Link to="/" onClick={toggleMenu}>Home</Link>
+        <Link to="/about" onClick={toggleMenu}>About Us</Link>
+        <Link to="/login" onClick={toggleMenu}>Login</Link>
       </div>
       <div className="title">
         <h1>UW Makers Exchange</h1>
@@ -20,4 +32,4 @@ export function Header() {
       </div>
     </header>
   );
-}
+} 
