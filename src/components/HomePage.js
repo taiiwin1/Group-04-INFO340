7428c4ac277d4309b1dde0f3ea2f8c487733bea2
taiiwin1 <tainnguyen2@gmail.com>
2024-05-20 02:05:04 -0700

    updated acessability

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -20,28 +20,32 @@ export function HomePage() {
 
   return (
     <div>
-      <div className="search-bar">
-        <input type="text" placeholder="Search..." />
-      </div>
-      <div className="category-dropdown">
-        <select defaultValue="" onChange={handleFilterChange}>
-          <option value="" disabled>Select Type</option>
-          <option value="Croches">Crocheting</option>
-          <option value="Custom Keycaps">Custom Keycaps</option>
-          <option value="Shirts">Shirts</option>
-          <option value="Bracelets">Bracelets</option>
-        </select>
-      </div>
-      <div className="card-container">
-        {filteredItems.map(item => (
-          <div key={item.id} className="card">
-            <img src={item.imgSrc} alt={item.title} />
-            <h2>{item.title}</h2>
-            <p>{item.description}</p>
+      <main>
+        <section>
+          <div className="search-bar">
+            <input type="text" placeholder="Search..." />
           </div>
-        ))}
-      </div>
-      <Footer />
+          <div className="category-dropdown">
+            <select defaultValue="" onChange={handleFilterChange}>
+              <option value="" disabled>Select Type</option>
+              <option value="Croches">Crocheting</option>
+              <option value="Custom Keycaps">Custom Keycaps</option>
+              <option value="Shirts">Shirts</option>
+              <option value="Bracelets">Bracelets</option>
+            </select>
+          </div>
+        </section>
+        <section className="card-container">
+          {filteredItems.map(item => (
+            <div key={item.id} className="card">
+              <img src={item.imgSrc} alt={item.title} />
+              <h2>{item.title}</h2>
+              <p>{item.description}</p>
+            </div>
+          ))}
+        </section>
+    </main>
+    <Footer />
     </div>
   );
 }
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -4,23 +4,22 @@ import '../style.css';
 
 export function LoginPage() {
   return (
-    <div className="login-page">
-      <div className="frame">
-        <div className="login-container">
-          <img src="img/Login-icon.png" alt="makers fair icon" />
-          <h1>Login</h1>
-          <form>
-            <label for="username">Username</label>
-            <input type="text" id="username" name="username" required />
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password" required />
-            <input type="submit" value="Log In" />
-          </form>
-        </div>
-      </div>
-      <footer>
-        © 2024 UW Informatics Students. All rights reserved.
-      </footer>
+    <div>
+      <main className="login-page">
+        <section className="frame">
+          <div className="login-container">
+            <img src="img/loginbackground.jpg" alt="Login Background" />
+            <h1>Login</h1>
+            <form>
+              <label htmlFor="username">Username</label>
+              <input type="text" id="username" name="username" required />
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" name="password" required />
+              <input type="submit" value="Login" />
+            </form>
+          </div>
+        </section>
+      </main>
       <Footer />
     </div>
   );
