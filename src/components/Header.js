ffdc69cb6444cd7b3fd5c0b72583360feacb2df4
taiiwin1 <tainnguyen2@gmail.com>
2024-05-21 00:55:15 -0700

    added dropdown manu interactivity

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,26 +3,21 @@ import '../style.css';
 import { Link } from 'react-router-dom';
 
 export function Header() {
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
   };
-
-  let navigationClass = 'navigation';
-  if (menuOpen) {
-    navigationClass += ' active';
-  }
 
   return (
     <header>
-      <div className="menu-icon" onClick={toggleMenu}>
+      <div className="menu-icon" onClick={toggleDropdown}>
         <img src="img/hamburger123.png" alt="the hamburger menu icon" />
       </div>
-      <div className={navigationClass}>
-        <Link to="/" onClick={toggleMenu}>Home</Link>
-        <Link to="/about" onClick={toggleMenu}>About Us</Link>
-        <Link to="/login" onClick={toggleMenu}>Login</Link>
+      <div className={`dropdown ${isDropdownOpen ? 'open' : ''}`}>
+        <Link to="/" onClick={toggleDropdown}>Home</Link>
+        <Link to="/about" onClick={toggleDropdown}>About Us</Link>
+        <Link to="/login" onClick={toggleDropdown}>Login</Link>
       </div>
       <div className="title">
         <h1>UW Makers Exchange</h1>
