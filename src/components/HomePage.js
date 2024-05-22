daa649e526ab1a64462c66d242a7d96ab478188d
taiiwin1 <tainnguyen2@gmail.com>
2024-05-21 18:58:08 -0700

    updated readme, added favicon, and removed map out of return

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,23 +1,7 @@
 import React, { useState } from 'react';
 import {Footer} from './Footer';
 import '../style.css';
-
-const items = [
-  { title: 'Olive green button-up', description: 'Olive green button-up sweater; work twice; size M', category: 'Clothing', imgSrc: 'img/shirt.jpg' },
-  { title: 'Snow white winter jacket', description: 'Snow white winter jacket; worn 4 times; size L', category: 'Clothing', imgSrc: 'img/00Jacket.webp' },
-  { title: 'Triple Desgined Braeclet', description: 'Comes with all 3 braecelets; entangled custom designs', category: 'Bracelets', imgSrc: 'img/bracelet.webp' },
-  { title: 'Kirby Keycap', description: 'Custom Kirby Moon keycap; fits on all keys', category: 'Custom Keycaps', imgSrc: 'img/keycaps2.webp' },
-  { title: 'Baby blue dinosaur', description: 'Baby blue dinosaur crochet; hand knit with homemade materials', category: 'Croches', imgSrc: 'img/crochet2.jpeg' },
-  { title: 'Brown bear crochet', description: 'Cute brown bear basket crochet', category: 'Croches', imgSrc: 'img/00crochets.jpeg' },
-  { title: 'Small pickle crochet', description: 'Small pickle holding sign crochet', category: 'Croches', imgSrc: 'img/01crochets.webp' },
-  { title: 'Baby elephant crochet', description: 'Mini baby elephant crochet', category: 'Croches', imgSrc: 'img/02crochets.webp' },
-  { title: 'Koi fish keycap', description: 'Colored koi fish keycap', category: 'Custom Keycaps', imgSrc: 'img/01keycaps.webp' },
-  { title: 'Starter pokemon keycaps', description: 'start pokemons: pikachu, charmander, squirtle, bulbasaur', category: 'Custom Keycaps', imgSrc: 'img/02keycaps.webp' },
-  { title: 'Connected hearts bracelet', description: '5 hearts entangled--3 bracelets', category: 'Bracelets', imgSrc: 'img/00bracelet.avif' },
-  { title: 'Marble bracelet', description: 'Marble collected from local shops in Hawaii', category: 'Bracelets', imgSrc: 'img/01bracelet.jpg' },
-  { title: 'Designer inspired bracelet', description: 'Elegant bracelet, inspired by existing bracelets', category: 'Bracelets', imgSrc: 'img/02bracelet.webp' },
-  { title: 'Denim Jacket', description: 'Denim jean jacket, long sleeve, with customed deisgned; brand new', category: 'Clothing', imgSrc: 'img/01clothes.avif' }
-];    
+import items from '../data/items.json'
 
 export function HomePage() {
   const [filter, setFilter] = useState('');
@@ -27,6 +11,14 @@ export function HomePage() {
   };
 
   const filteredItems = items.filter(item => item.category.includes(filter));
+
+  const filteredItems2 = filteredItems.map(item => (
+    <div key={item.id} className="card">
+      <img src={item.imgSrc} alt={item.title} />
+      <h2>{item.title}</h2>
+      <p>{item.description}</p>
+    </div>
+  ));
 
   return (
     <div>
@@ -46,13 +38,7 @@ export function HomePage() {
           </div>
         </section>
         <section className="card-container">
-          {filteredItems.map(item => (
-            <div key={item.id} className="card">
-              <img src={item.imgSrc} alt={item.title} />
-              <h2>{item.title}</h2>
-              <p>{item.description}</p>
-            </div>
-          ))}
+          {filteredItems2}
         </section>
     </main>
     <Footer />
